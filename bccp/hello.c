b1e8852b43793087f8941b63d37267da4ea8614d
dtice <dillon.tice@gmail.com>
2019-10-15 02:08:14 -0600

    .

--- bccp/hello.c
+++ bccp/hello.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <math.h>
 
 enum direction{TO_BRIDGER, TO_BOZEMAN};
 const char *directions[] = {"To Bridger", "To Bozeman"};
@@ -10,16 +11,25 @@ const char *directions[] = {"To Bridger", "To Bozeman"};
 typedef struct
 {
 	enum direction dir;
+	//pthread_t *tid;
+	int tid;
 
 } car;
+
+typedef struct
+{
+	int cars_on_oneway;
+	enum direction dir;
+	int to_bridger;
+	int to_bozeman;
+	int cars_passed;
+} sim_state;
 
 int MAXCARS;
 int NUMCARS;
 
 pthread_mutex_t lock;
-
-// cars_on_oneway acts as a semaphore
-int cars_on_oneway;
+sim_state state;
 
 int *parse_args(int argc, char **argv)
 {
@@ -43,47 +53,109 @@ int *parse_args(int argc, char **argv)
 	return parsed_args;
 }
 
-void ArriveOneWay(void * direction)
+void ArriveBridgerOneWay(void *c)
 {
-	pthread_mutex_lock(&lock);
-	int dir = (int)*((enum direction*)direction);
-	switch(dir)
+	car *carAd = (car*)c;
+	int tid = carAd->tid;
+	printf("[Car #%d] Arrived at One-Way\n", tid);
+	switch((int)carAd->dir)
 	{
 		case 0:
-			printf("\n Traversing One-Way To Bridger\n");
-			sleep(1);
+			state.to_bridger++;
 			break;
 		case 1:
-			printf("\n Traversing One-Way to Bozeman\n");
-			sleep(1);
+			state.to_bozeman++;
 			break;
 	}
+	int can_proceed = 0;
+	int i = 0;
+	while(can_proceed != 1)
+	{
+		int err = pthread_mutex_lock(&lock);
+		if(err != 0)
+		{
+			printf("error obtaining mutex lock: %d\n", err);
+			return;
+		}
+		// Obtained lock
+		if(state.cars_on_oneway < MAXCARS)
+		{
+			printf("\n[Car #%d] Can proceed\n", tid);
+			can_proceed = 1;
+			state.cars_on_oneway++;
+		}
+		else
+		{
+			if(i % 5000000 == 0)
+			{
+				printf("\n[Car #%d] Waiting. Road is full\n", tid);
+			}
+		}
+		pthread_mutex_unlock(&lock);
+		i++;
+	}
+}
+
+void OnBridgerOneWay(void *c)
+{
+	car *carAd = (car *)c;
+	int tid = carAd->tid;
+	printf("[Car #%d] On Bridger One-Way Heading %s\n", tid, directions[carAd->dir]);
+	//int randSec = floor(5 * rand() / ((double)RAND_MAX));
+	//sleep(randSec);
+	sleep(1);
+	int err = pthread_mutex_lock(&lock);
+	if(err != 0)
+	{
+		printf("error obtaining mutex lock\n");
+		return;
+	}
+	pthread_mutex_unlock(&lock);
+}
+
+void ExitBridgerOneWay(void *c)
+{
+	car *carAd = (car *)c;
+	int tid = carAd->tid;
+	printf("[Car #%d] Exiting Bridger One-Way Heading %s\n", tid, directions[carAd->dir]);
+	int err = pthread_mutex_lock(&lock);
+	if(err != 0)
+	{
+		printf("error obtaining mutex lock\n");
+		return;
+	}
+	state.cars_on_oneway--;
+	state.cars_passed++;
 	pthread_mutex_unlock(&lock);
 }
 
 void *OneVehicle(void *c)
 {
-	//int *argPtr = c;
 	car *car_addr = (car*)c;
-	car curCar = *car_addr;
-	//TODO: Thread ID
-	//int tid;
-	//printf("\nBEGIN THREAD #%d-----\n", tid);
-	printf("Direction: %s\n", directions[curCar.dir]);
-	//ArriveBridgerOneWay(direction);
-	//OnBridgerOneWay(direction);
-	//ExitBridgerOneWay(direction);
-	printf("END THREAD-----\n");
+	int tid = car_addr->tid;
+	//printf("\nBEGIN THREAD #%d-----\n", car_addr->tid);
+	printf("[Car #%d] Heading %s\n", tid, directions[car_addr->dir]);
+	ArriveBridgerOneWay(c);
+	OnBridgerOneWay(c);
+	ExitBridgerOneWay(c);
+	//printf("END THREAD #%d-----\n", car_addr->tid);
 }
 
 int main(int argc, char **argv) 
 {
+	int err = pthread_mutex_init(&lock, NULL);
+	if(err != 0)
+	{
+		printf("\n Mutex Lock Failed: %d\n", err);
+		return 1;
+	}
 	int* parsed_args = parse_args(argc, argv);
 
-	NUMCARS = parsed_args[0]; // Number of cars to simulate
-	MAXCARS = parsed_args[1]; // Maximum amount of cars allowed on one-way stretch
+	// Initialize number of cars to be simulated
+	NUMCARS = parsed_args[0]; 
 
-	cars_on_oneway = MAXCARS;
+	// Initialize MAXCARS and semaphore value
+	MAXCARS = parsed_args[1];
 
 	// printf("Max Cars: %d\nNum Cars: %d\n", MAXCARS, NUMCARS);
 
@@ -91,12 +163,13 @@ int main(int argc, char **argv)
 
 	cars = (pthread_t *)malloc(NUMCARS * sizeof(pthread_t )); //Initialize thread pointers
 
-	car carArray[NUMCARS]; // Array for random direction values of each thread
+	car carArray[NUMCARS]; // Array for car structs
 
 	srand(time(0));	
 
 	for(int i = 0; i < NUMCARS; i++)
 	{
+		//Initialize NUMCARS with randomized directions
 		int randBit = rand() % 2;
 
 		enum direction dir;
@@ -112,8 +185,10 @@ int main(int argc, char **argv)
 
 		car *curCar = malloc(sizeof(*curCar));
 		curCar->dir = dir;
+		//curCar->tid = &cars[i];
+		curCar->tid = i;
 		carArray[i] = *curCar;
-		pthread_create(&cars[i], NULL, OneVehicle, &carArray[i]);
+		pthread_create(&cars[i], NULL, OneVehicle, (void *)&carArray[i]);
 		free(curCar);
 	}
 
@@ -121,6 +196,8 @@ int main(int argc, char **argv)
 	{
 		pthread_join(cars[i], NULL);
 	}
+
+	pthread_mutex_destroy(&lock);
 
 	return 0;
 }
